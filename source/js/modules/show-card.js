7854f30e109acf581c17f71e845748da3f33acc7
Polina <pmilekhina@gmail.com>
2023-01-13 16:37:47 +0400

    Менякт логику открытия карточек

--- source/js/modules/show-card.js
+++ source/js/modules/show-card.js
@@ -1,31 +1,63 @@
 const cards = document.querySelectorAll('.trainers__item');
 
+const clickOnCard = (e) => {
+  cards.forEach((info) => {
+    const cardsInfo = info.querySelector('.trainers__info');
+    const cardsTitle = info.querySelector('h3');
+    cardsInfo.classList.remove('flex');
+    cardsTitle.classList.remove('none');
+  });
+
+  const card = e.currentTarget;
+
+  const cardInfo = card.querySelector('.trainers__info');
+  const cardPhoto = card.querySelector('.trainers__photo');
+  const cardTitle = cardPhoto.querySelector('h3');
+
+  cardInfo.classList.add('flex');
+  cardTitle.classList.add('none');
+};
+
 const showCardInfo = () => {
   if (cards) {
     cards.forEach((item) => {
-      item.addEventListener('click', () => {
-        cards.forEach((info) => {
-          const cardsInfo = info.querySelector('.trainers__info');
-          const cardsTitle = info.querySelector('h3');
-          cardsInfo.style.display = 'none';
-          cardsTitle.style.display = 'block';
-        });
-        let currentCard = item;
-        const cardInfo = currentCard.querySelector('.trainers__info');
-        const cardPhoto = currentCard.querySelector('.trainers__photo');
-        const cardTitle = cardPhoto.querySelector('h3');
 
-        cardInfo.style.display = 'flex';
-        cardTitle.style.display = 'none';
-      });
+      item.addEventListener('click', clickOnCard);
+    });
+  }
+};
+const stopShowCardInfoByClick = () => {
+  if (cards) {
+
+
+    cards.forEach((item) => {
+      const cardsInfo = item.querySelector('.trainers__info');
+      const cardsTitle = item.querySelector('h3');
+
+      cardsInfo.classList.remove('flex');
+      cardsTitle.classList.remove('none');
+
+      item.removeEventListener('click', clickOnCard);
     });
   }
 };
 
 const onCardClick = () => {
+  let isByClick = false;
+
+  if (window.innerWidth <= 1199) {
+    isByClick = true;
+    showCardInfo();
+
+  }
+
   window.addEventListener('resize', () => {
-    if (window.innerWidth <= 1199) {
+    if (window.innerWidth <= 1199 && !isByClick) {
       showCardInfo();
+      isByClick = true;
+    } else if (window.innerWidth > 1199 && isByClick) {
+      stopShowCardInfoByClick();
+      isByClick = false;
     }
   });
 };
